8a38de0abf5e01078f6274336b46c6a1d3eced92
sindain <sindain89@hotmail.com>
2015-10-19 16:27:29 -0500

    Changing things
    DO NOT MERGE TO STAGE!!!!

---
+++ NeonHell/Project/Assets/Scripts/Player/PlayerController1.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController1 : MonoBehaviour {
+
+	private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate(){
+		float hMove = Input.GetAxis ("Horizontal");
+
+		//rb.AddRelativeTorque (new Vector3(0.0f, hMove * 0.5f, 0.0f));
+
+		rb.AddRelativeForce(new Vector3(0f,0f,500.0f));
+	}
+}
