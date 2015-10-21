c00a56ecc4bff206aa80a2f430d47d2fd0039a99
TheZanester <greyz0397@my.uwstout.edu>
2015-10-21 01:42:27 -0500

    Bomber Spawner
    Added something to make the bomber move to a position when you round a
    corner

---
+++ NeonHell/Project/Assets/Scripts/BomberSpawner.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BomberSpawner : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Player") {
+			GameObject.Find ("Bomber 1").transform.position = transform.position;
+			GameObject.Find ("Bomber 1").transform.rotation = transform.rotation;
+		}
+	}
+}
