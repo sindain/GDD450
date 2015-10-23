dd8791efb0e3e86e8468ab6d4e6732ee2e98d892
sindain <sindain89@hotmail.com>
2015-10-23 16:23:40 -0500

    10/20/15 end of class updates

--- NeonHell/Project/Assets/Scripts/Player/PlayerController1.cs
+++ NeonHell/Project/Assets/Scripts/Player/PlayerController1.cs
@@ -27,6 +27,7 @@ public class PlayerController1 : MonoBehaviour {
 	}
 
 	void FixedUpdate(){
+		Vector3 newRotation;
 		print (rb.velocity.magnitude);
 		if (Physics.Raycast (transform.position, - this.transform.up, 4.0f)) {
 			rb.drag = 1;
@@ -34,11 +35,15 @@ public class PlayerController1 : MonoBehaviour {
 			Vector3 forwardForce = transform.forward * maxVelocity * 50.0f * Input.GetAxis ("Vertical");
 			forwardForce = forwardForce * Time.deltaTime * rb.mass;
 			rb.AddForce (forwardForce);
-			rb.AddRelativeForce(Input.GetAxis("Strafe") * new Vector3(strafeAcceleration,0.0f,0.0f) * Time.deltaTime * rb.mass);
-			if((transform.rotation * rb.velocity).z < minVelocity)
-				rb.AddRelativeForce(new Vector3(0.0f,0.0f, minVelocity * rb.drag * 50 * Time.deltaTime * rb.mass));
-		} else
+			rb.AddRelativeForce (Input.GetAxis ("Strafe") * new Vector3 (strafeAcceleration, 0.0f, 0.0f) * Time.deltaTime * rb.mass);
+			//if ((transform.rotation * rb.velocity).z < minVelocity)
+				//rb.AddRelativeForce (new Vector3 (0.0f, 0.0f, minVelocity * rb.drag * 50 * Time.deltaTime * rb.mass));
+		} else {
 			rb.drag = 0;
+			newRotation = transform.eulerAngles;
+			newRotation.x = Mathf.SmoothDampAngle(newRotation.x, 0.0f, ref rotationVelocity, turnRotationSeekSpeed);
+			transform.eulerAngles = newRotation;
+		}
 
 		
 
@@ -47,9 +52,9 @@ public class PlayerController1 : MonoBehaviour {
 		turnTorque = turnTorque * Time.deltaTime * rb.mass;
 		rb.AddTorque(turnTorque);
 
-		Vector3 newRotation = transform.eulerAngles;
+		newRotation = transform.eulerAngles;
 		newRotation.z = Mathf.SmoothDampAngle(newRotation.z, Input.GetAxis("Horizontal") * -turnRotationAngle, ref rotationVelocity, turnRotationSeekSpeed);
-		transform.eulerAngles = newRotation;
+		//transform.eulerAngles = newRotation;
 
 	}
 }
