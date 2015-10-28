b44436f9dcaf60728b4bb81282a0d7f9fc69dc81
jonyboy199415 <jonnyboy199413@yahoo.com>
2015-10-28 15:16:12 -0500

    kill plane script

---
+++ NeonHell/Project/Assets/Scripts/KillPlaneScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillPlaneScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.tag == "Player") {
+			col.transform.position= new Vector3(0,10,0);
+		}
+	}
+}
