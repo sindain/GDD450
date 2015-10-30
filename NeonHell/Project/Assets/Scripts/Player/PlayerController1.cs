4b21e78a61e4cf4225318e63e7e078d84df096ff
CausticTarget <grantbordson@gmail.com>
2015-10-30 14:02:19 -0500

    Added NetworkChecks

--- NeonHell/Project/Assets/Scripts/Player/PlayerController1.cs
+++ NeonHell/Project/Assets/Scripts/Player/PlayerController1.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
-
-public class PlayerController1 : MonoBehaviour {
+using UnityEngine.Networking;
+public class PlayerController1 : NetworkBehaviour {
 	public float maxVelocity;
 	public float minVelocity;
 	public float rotationRate;
@@ -9,15 +9,18 @@ public class PlayerController1 : MonoBehaviour {
 	public float jumpStrength;
 	public float turnRotationAngle;
 	public float turnRotationSeekSpeed;
-
+    public bool canMove;
 
 	private float rotationVelocity;
 	private float groundAngleVelocity;
 
+
 	private Rigidbody rb;
 	// Use this for initialization
 	void Start () {
+        
 		rb = GetComponent<Rigidbody> ();
+        canMove = false;
 	}
 	
 	// Update is called once per frame
@@ -25,28 +28,36 @@ public class PlayerController1 : MonoBehaviour {
 	
 	}
 
-	void FixedUpdate(){
-		Vector3 newRotation;
-		print (rb.velocity.magnitude);
-		if (Physics.Raycast (transform.position, - this.transform.up, 4.0f)) {
-			rb.drag = 1;
-			Vector3 forwardForce = transform.forward * maxVelocity * 50.0f * Input.GetAxis ("Vertical");
-			forwardForce = forwardForce * Time.deltaTime * rb.mass;
-			rb.AddForce (forwardForce);
-			//rb.AddRelativeForce (Input.GetAxis ("Strafe") * new Vector3 (strafeAcceleration, 0.0f, 0.0f) * Time.deltaTime * rb.mass); //things
-			//if ((transform.rotation * rb.velocity).z < minVelocity)
-				//rb.AddRelativeForce (new Vector3 (0.0f, 0.0f, minVelocity * rb.drag * 50 * Time.deltaTime * rb.mass));
-		} else {
-			rb.drag = 0;
-			newRotation = transform.eulerAngles;
-			newRotation.x = Mathf.SmoothDampAngle(newRotation.x, 0.0f, ref rotationVelocity, turnRotationSeekSpeed);
-			newRotation.z = Mathf.SmoothDampAngle(newRotation.z, 0.0f, ref rotationVelocity, turnRotationSeekSpeed);
-			transform.eulerAngles = newRotation;
-		}
+    void FixedUpdate()
+    {
+        if (canMove && PlayerPrefs.GetFloat("start") == 1)
+        {
 
-		Vector3 turnTorque = transform.up * rotationRate * Input.GetAxis("Horizontal");
-		turnTorque = turnTorque * Time.deltaTime * rb.mass;
-		rb.AddTorque(turnTorque);
-	}
+            Vector3 newRotation;
+            print(rb.velocity.magnitude);
+            if (Physics.Raycast(transform.position, -this.transform.up, 4.0f))
+            {
+                rb.drag = 1;
+                Vector3 forwardForce = transform.forward * maxVelocity * 50.0f * Input.GetAxis("Vertical");
+                forwardForce = forwardForce * Time.deltaTime * rb.mass;
+                rb.AddForce(forwardForce);
+                //rb.AddRelativeForce (Input.GetAxis ("Strafe") * new Vector3 (strafeAcceleration, 0.0f, 0.0f) * Time.deltaTime * rb.mass);
+                //if ((transform.rotation * rb.velocity).z < minVelocity)
+                //rb.AddRelativeForce (new Vector3 (0.0f, 0.0f, minVelocity * rb.drag * 50 * Time.deltaTime * rb.mass));
+            }
+            else
+            {
+                rb.drag = 0;
+                newRotation = transform.eulerAngles;
+                newRotation.x = Mathf.SmoothDampAngle(newRotation.x, 0.0f, ref rotationVelocity, turnRotationSeekSpeed);
+                newRotation.z = Mathf.SmoothDampAngle(newRotation.z, 0.0f, ref rotationVelocity, turnRotationSeekSpeed);
+                transform.eulerAngles = newRotation;
+            }
 
+            Vector3 turnTorque = transform.up * rotationRate * Input.GetAxis("Horizontal");
+            turnTorque = turnTorque * Time.deltaTime * rb.mass;
+            rb.AddTorque(turnTorque);
+        }
+
+    }
 }
