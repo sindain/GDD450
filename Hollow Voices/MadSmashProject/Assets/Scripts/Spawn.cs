b79b9cc82a3271fc0cf6d65e71e89cd5ccb356d2
Huckmaster <paulieboywendt@gmail.com>
2015-11-15 17:11:23 -0600

    spawn random object

--- Hollow Voices/MadSmashProject/Assets/Scripts/Spawn.cs
+++ Hollow Voices/MadSmashProject/Assets/Scripts/Spawn.cs
@@ -17,13 +17,10 @@ public class Spawn : MonoBehaviour {
 
 		if (rand == 0) {
 			Instantiate (item, spawnPoint.position, spawnPoint.rotation);
-			//Instantiate (item, new Vector3 (428, 508, -668), Quaternion.identity);
 		} else if (rand == 1) {
 			Instantiate (item1, spawnPoint.position, spawnPoint.rotation);
-			//Instantiate (item1, new Vector3 (428, 508, -668), Quaternion.identity);
 		} else if (rand == 2) {
 			Instantiate (item2, spawnPoint.position, spawnPoint.rotation);
-			//Instantiate (item2, new Vector3 (428, 508, -668), Quaternion.identity);
 		} else if (rand == 3) {
 			Instantiate (item3, spawnPoint.position, spawnPoint.rotation);
 		}
